db49ae1dacab1cd75569b6b7d85743b8854d6325
ZJCQ301 <125265493+ZJCQ301@users.noreply.github.com>
2026-06-12 05:50:06 +0800

    Update zygisk.hpp

--- zygisk/zygisk.hpp
+++ zygisk/zygisk.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <jni.h>
-#include <dlfcn.h>
+#include <cstdio>
 
 namespace zygisk {
 
@@ -11,31 +11,27 @@ struct ServerSpecializeArgs;
 
 class ModuleBase {
 public:
-    virtual void onLoad(Api* api, JNIEnv* env) = 0;
-    virtual void preAppSpecialize(AppSpecializeArgs* args) {}
-    virtual void postAppSpecialize(const AppSpecializeArgs* args) {}
-    virtual void preServerSpecialize(ServerSpecializeArgs* args) {}
-    virtual void postServerSpecialize(const ServerSpecializeArgs* args) {}
+    virtual void onLoad(Api *api, JNIEnv *env) = 0;
+    virtual void preAppSpecialize(AppSpecializeArgs *args) {}
+    virtual void postAppSpecialize(const AppSpecializeArgs *args) {}
+    virtual void preServerSpecialize(ServerSpecializeArgs *args) {}
+    virtual void postServerSpecialize(const ServerSpecializeArgs *args) {}
     virtual ~ModuleBase() = default;
 };
 
 struct Api {
     virtual const char* getProcessName() = 0;
-    virtual void pltHookRegister(const char* regex, const char* symbol, void* newFunc, void** oldFunc) = 0;
+    virtual void pltHookRegister(const char *regex, const char *symbol, void *newFunc, void **oldFunc) = 0;
     virtual void pltHookCommit() = 0;
+    virtual void registerCompanion(void *entry) = 0;  // 关键！
 };
 
-struct AppSpecializeArgs {
-    void* padding[10];
-};
-
-struct ServerSpecializeArgs {
-    void* padding[6];
-};
+struct AppSpecializeArgs { void *padding[10]; };
+struct ServerSpecializeArgs { void *padding[6]; };
 
 } // namespace zygisk
 
 #define REGISTER_ZYGISK_MODULE(className) \
-    extern "C" __attribute__((visibility("default"))) zygisk::ModuleBase* zygisk_module_create() { \
+    extern "C" __attribute__((visibility("default"))) zygisk::ModuleBase *zygisk_module_create() { \
         return new className(); \
 }
