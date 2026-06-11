9b74f74cf9e56dc6a776fd3dd02e9673949f4aa1
ZJCQ301 <125265493+ZJCQ301@users.noreply.github.com>
2026-06-12 04:38:22 +0800

    Update zygisk.hpp

--- zygisk/zygisk.hpp
+++ zygisk/zygisk.hpp
@@ -4,22 +4,41 @@
 #include <cstdio>
 #include <string>
 #include <dlfcn.h>
+#include <android/log.h>
 
 namespace zygisk {
 
+    struct Api;
+    struct AppSpecializeArgs;
+    struct ServerSpecializeArgs;
+
     class ModuleBase {
     public:
-        virtual void onLoad(void* api, JNIEnv* env) = 0;
-        virtual void preAppSpecialize(void* args) {}
-        virtual void postAppSpecialize(const void* args) {}
-        virtual void preServerSpecialize(void* args) {}
-        virtual void postServerSpecialize(const void* args) {}
+        virtual void onLoad(Api *api, JNIEnv *env) = 0;
+        virtual void preAppSpecialize(AppSpecializeArgs *args) {}
+        virtual void postAppSpecialize(const AppSpecializeArgs *args) {}
+        virtual void preServerSpecialize(ServerSpecializeArgs *args) {}
+        virtual void postServerSpecialize(const ServerSpecializeArgs *args) {}
         virtual ~ModuleBase() = default;
+    };
+
+    struct Api {
+        virtual const char* getProcessName() = 0;
+        virtual void pltHookRegister(const char *regex, const char *symbol, void *newFunc, void **oldFunc) = 0;
+        virtual void pltHookCommit() = 0;
+    };
+
+    struct AppSpecializeArgs {
+        void* padding[10];
+    };
+
+    struct ServerSpecializeArgs {
+        void* padding[6];
     };
 
 }
 
 #define REGISTER_ZYGISK_MODULE(className) \
-    extern "C" __attribute__((visibility("default"))) zygisk::ModuleBase* zygisk_module_create() { \
+    extern "C" __attribute__((visibility("default"))) zygisk::ModuleBase *zygisk_module_create() { \
         return new className(); \
 }
